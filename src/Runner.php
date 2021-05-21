87ee6ef715d36f6d7983ce49d03d12ffdcbe46a9
Andy Fragen <andy@thefragens.com>
2021-05-21 08:31:14 -0700

    prevent redeclaration of class, use `include_once`

--- src/Runner.php
+++ src/Runner.php
@@ -18,10 +18,10 @@ class Runner {
 			define( 'WP_CLI_ROOT', $vendorDir . '/wp-cli/wp-cli' );
 		}
 
-		include WP_CLI_ROOT . '/php/utils.php';
-		include WP_CLI_ROOT . '/php/dispatcher.php';
-		include WP_CLI_ROOT . '/php/class-wp-cli.php';
-		include WP_CLI_ROOT . '/php/class-wp-cli-command.php';
+		include_once WP_CLI_ROOT . '/php/utils.php';
+		include_once WP_CLI_ROOT . '/php/dispatcher.php';
+		include_once WP_CLI_ROOT . '/php/class-wp-cli.php';
+		include_once WP_CLI_ROOT . '/php/class-wp-cli-command.php';
 
 		\WP_CLI\Utils\load_dependencies();
 
